a99b2aed4dbe175d6dbb25dc87ef093683bf4095
contactcallai <contact.callai@gmail.com>
2026-02-27 05:48:21 +0100

    Fix process.env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const { callOpenAI, construirPromptAnalisisTodas, evaluarPartidoUnico, generarPrimeraRonda } = require('./public/generador-cuadros-cabeza-ia-pista.js');
+const { callOpenAI, construirPromptAnalisisTodas, evaluarPartidoUnico, generarPrimeraRonda } = require('./generador-cuadros-cabeza-ia-pista.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
